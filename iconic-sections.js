49203c947220176a35b38510992f1f5b540db2d3
Nikita Patel <nikita@arhamwebworks.com>
2024-09-26 15:39:00 +0530

    small issue fixed.
    small issue fixed

--- iconic-sections.js
+++ iconic-sections.js
@@ -17,12 +17,6 @@
         scriptUrl: "https://cdn.jsdelivr.net/gh/ArhamWebWorks/iconic-cdn/component-iconic-rotatingbar.js",
         styleUrl: "",
       },
-      {
-        triggerBy: "tag",
-        trigger: "branding-text",
-        scriptUrl: "https://cdn.jsdelivr.net/gh/ArhamWebWorks/iconic-cdn/component-iconic-branding-text.js",
-        styleUrl: "",
-      },
       // Add more components here if needed
   ];
 
