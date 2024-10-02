c9b7ce95b04d7e608e5bce19cfea5d82ed93a34b
Nikita Patel <nikita@arhamwebworks.com>
2024-10-02 16:58:09 +0530

    minify js
    minify js

--- component-iconic-modal.js
+++ component-iconic-modal.js
@@ -4,22 +4,4 @@
 //   "description": "Copying in any form is strictly prohibited. Any instance of copying will be subject to legal action and accountability under the law."
 // }
 
-function showModal(modal) {
-  modal.classList.add("show-modal");
-  document.body.classList.add("iconic-overflow-hidden");
-  modal.dispatchEvent(new Event("shown.custom.modal"));
-}
-
-function hideModal(modal) {
-  modal.classList.remove("show-modal");
-  document.body.classList.remove("iconic-overflow-hidden");
-  modal.dispatchEvent(new Event("hidden.custom.modal"));
-}
-
-function closeModalOnClickOutside(modal) {
-  modal.addEventListener("click", (event) => {
-    if (event.target === modal) {
-      hideModal(modal);
-    }
-  });
-}
+function showModal(o){o.classList.add("show-modal"),document.body.classList.add("iconic-overflow-hidden"),o.dispatchEvent(new Event("shown.custom.modal"))}function hideModal(o){o.classList.remove("show-modal"),document.body.classList.remove("iconic-overflow-hidden"),o.dispatchEvent(new Event("hidden.custom.modal"))}function closeModalOnClickOutside(o){o.addEventListener("click",(d=>{d.target===o&&hideModal(o)}))}
